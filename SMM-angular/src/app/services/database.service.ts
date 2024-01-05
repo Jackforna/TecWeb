a9d83cf2a196a76fcd8743792fe17a4a6d251f9a
GabboBTT <gabriel.betti@studio.unibo.it>
2024-01-05 11:47:33 +0100

    Fix database in user settings, ma problema con aggiornamento istantaneo non funzionante

--- SMM-angular/src/app/services/database.service.ts
+++ SMM-angular/src/app/services/database.service.ts
@@ -22,6 +22,11 @@ export class DatabaseService {
     return this.http.get('/get-users');
   }
 
+  updateUserProfile(userId: string, updateData: any): Observable<any> {
+    return this.http.put(`http://localhost:8080/update-user/${userId}`, updateData);
+  }
+  
+
 
   //Da sistemare
   insertUser(uid: string, email: string, username: string) {
