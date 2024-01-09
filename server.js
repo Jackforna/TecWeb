259c0d8ebd8f7ec81c2afd1716c86e92e67db3bb
Jackforna <119734118+Jackforna@users.noreply.github.com>
2024-01-09 17:41:56 +0100

    i

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const dbUrl = 'mongodb://root:example@localhost:27017';
 const client = new MongoClient(dbUrl);
 const dbName = 'my-mongo-container';
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit:'100mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/squealer-app', express.static(path.join(__dirname, 'my-react-app/build')));
 
